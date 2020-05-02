9c8a4f7ef68377199a99d9dcac78624d57355642
Alasdair Lumsden <alasdair@Alasdairs-Mac-mini.local>
2020-05-02 15:55:50 +0100

    Sloppy workmanship

--- data.tf
+++ data.tf
@@ -2,10 +2,26 @@ data "template_file" "cc-k8s-master" {
     template = "${path.module}/g2-ubuntu18-k8s-master.tpl"
     vars = {
       kubernetes_version = "${var.kubernetes_version}"
+      calico_version = "${var.calico_version}"
+      metallb_version = "${var.metallb_version}"
       rook_version = "${var.rook_version}"
+      ceph_version = "${var.ceph_version}"
+      kured_version = "${var.kured_version}"
+      dashboard_version = "${var.dashboard_version}"
+      helm_version = "${var.helm_version}"
     }
 }
 
 data "template_file" "cc-k8s-worker" {
-    filename = "${path.module}/g2-ubuntu18-k8s-worker.tpl"
+    template = "${path.module}/g2-ubuntu18-k8s-worker.tpl"
+    vars = {
+      kubernetes_version = "${var.kubernetes_version}"
+      calico_version = "${var.calico_version}"
+      metallb_version = "${var.metallb_version}"
+      rook_version = "${var.rook_version}"
+      ceph_version = "${var.ceph_version}"
+      kured_version = "${var.kured_version}"
+      dashboard_version = "${var.dashboard_version}"
+      helm_version = "${var.helm_version}"
+    }
 }
